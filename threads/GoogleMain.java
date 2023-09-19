ecfd6dbad20616073d8eb321eb9478cd233ef5a1
ThiagoWesley18 <tw180896@gmail.com>
2023-09-19 17:52:52 -0400

    thread

--- threads/GoogleMain.java
+++ threads/GoogleMain.java
@@ -1,8 +1,59 @@
 package threads;
 
-public class GoogleMain {
+public class GoogleMain extends Thread{
+    String palavra = null;
+    ListaInvertida lista;
+
+    public GoogleMain(ListaInvertida lista){
+        this.lista = lista;
+        this.start();
+    }
+    public GoogleMain(String palavra , ListaInvertida lista){
+        this.palavra = palavra;
+        this.lista = lista;
+        this.start();
+    } 
+
+    public void run(){
+        if(palavra != null){
+            System.out.println(lista.busca(palavra));
+        }else{
+            System.out.println(lista.toString());
+        }
+    }
+
     public static void main(String[] args) {
+        ListaInvertida lista = new ListaInvertida();
         
+
+        lista.insere("force", "document1.txt");
+        lista.insere("force", "document2.txt");
+        lista.insere("force", "document3.txt");
+        lista.insere("always", "document1.txt");
+        lista.insere("one", "document3.txt");
+        lista.insere("is", "document2.txt");
+        lista.insere("is", "document3.txt");
+        lista.insere("be", "document1.txt");
+        lista.insere("will", "document1.txt");
+        lista.insere("you", "document1.txt");
+        lista.insere("you", "document2.txt");
+        lista.insere("the", "document1.txt");
+        lista.insere("the", "document2.txt");
+        lista.insere("the", "document3.txt");
+        lista.insere("remember", "document1.txt");
+        lista.insere("this", "document3.txt");
+        lista.insere("strong", "document2.txt");
+        lista.insere("strong", "document3.txt");
+        lista.insere("with", "document1.txt");
+        lista.insere("with", "document2.txt");
+        lista.insere("with", "document3.txt");
+        
+        new GoogleMain("this", lista);
+        new GoogleMain("force", lista);
+        new GoogleMain("the", lista);
+        new GoogleMain(lista);
+        
+
     }
     
 }
--- threads/ListaInvertida.java
+++ threads/ListaInvertida.java
@@ -4,5 +4,37 @@ import java.util.Hashtable;
 import java.util.LinkedList;
 
 public class ListaInvertida {
-    Hashtable<String, LinkedList<String>> tabela = new Hashtable<>();
+   private Hashtable<String, LinkedList<String>> tabela;
+
+    public ListaInvertida(){
+        tabela = new Hashtable<>();
+    }
+
+    public boolean insere(String palavra, String documento){
+        LinkedList<String> lista;
+        if (tabela.get(palavra) == null){
+            lista = new LinkedList<>();
+            lista.addLast(documento);
+            tabela.put(palavra,lista);
+            return true;
+        }else{
+            lista = tabela.get(palavra);
+            if (lista != null){
+                if(lista.contains(documento) == false){
+                    lista.addLast(documento);
+                    tabela.put(palavra,lista);
+                    return true;
+                }else
+                    return false;
+            }else
+                return false;
+        }
+    }
+
+    public LinkedList<String> busca(String palavra){
+        return tabela.get(palavra);
+    }
+    public String toString(){
+        return tabela.toString();
+    }
 }
